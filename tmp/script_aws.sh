802500a28f179fda39a287d19cfb0883557d17ed
OmarJhon9872 <omarjonax0@gmail.com>
2022-08-07 23:27:01 -0500

    02 commit archivo

--- tmp/script_aws.sh
+++ tmp/script_aws.sh
@@ -6,7 +6,7 @@ DB_PASSWORD=$2
 DB_NAME=$3
 AWS_SECRET=$4
 BUCKET_NAME=$5
-
+ 
 mysqldump --column-statistics=0 -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/db-$DATE.sql && \
 export AWS_ACCESS_KEY_ID=AKIAY5BMGQ2CG4L2H6Q2 && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
